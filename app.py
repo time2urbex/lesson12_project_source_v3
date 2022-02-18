626aa247e4a5688a7c02770994a054c885efde97
kushedow <kushedow@gmail.com>
2022-02-18 17:24:09 +0300

    version 3 changes

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import logging
+
 from flask import Flask, request, render_template, send_from_directory
 # from functions import ...
+from main.views import main_blueprint
 
 POST_PATH = "posts.json"
 UPLOAD_FOLDER = "uploads/images"
@@ -7,18 +10,13 @@ UPLOAD_FOLDER = "uploads/images"
 app = Flask(__name__)
 
 
-@app.route("/")
-def page_index():
-    pass
+app.register_blueprint(main_blueprint)
+app.register_blueprint(loader_blueprint)
+logging.basicConfig(filename="basic.log", level=logging.INFO)
 
 
-@app.route("/tag")
+@app.route("/list")
 def page_tag():
-    pass
-
-
-@app.route("/post", methods=["GET", "POST"])
-def page_post_create():
     pass
 
 
---
+++ main/views.py
@@ -0,0 +1,22 @@
+import logging
+
+from flask import Blueprint, render_template, request
+
+main_blueprint = Blueprint('main_blueprint' __name__, template_folder='templates')
+
+@main_blueprint.route('/')
+def main_page():
+    return render_template('index.html')
+
+@main_blueprint.route('/search/')
+def search_page():
+    search_query = request.args.get('s', '')
+    logging.info('Выполняю поиск')
+    try:
+        posts = get_posts_by_word(search_query)
+    except FileNotFoundError:
+        return 'Файл не найден'
+
+    except JSONDecoderError:
+        return 'Невалидный файл'
+    return render_template('post_list.html', query=search_query, posts=posts)
